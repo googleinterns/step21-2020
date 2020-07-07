34a5a8b01ba05fa4c34fd790c5af4813a7043e67
Laila Gamaleldin <laila_gamaleldin@brown.edu>
2020-07-07 16:33:03 +0000

    datastore user information

--- step/src/main/java/com/google/sps/DatabaseHandler.java
+++ step/src/main/java/com/google/sps/DatabaseHandler.java
@@ -19,9 +19,21 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.NoSuchElementException;
+import javax.servlet.http.HttpServletRequest;
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
 
 public final class DatabaseHandler {
   
+  // Accessing Datastore
+  private static DatastoreService datastore =
+         DatastoreServiceFactory.getDatastoreService();
+
   // TODO: replace with Datastore
   private static Map<Long, User> userMap = new HashMap<>();
   private static Map<Long, Collection<Notification>> notificationMap = new HashMap<>();
@@ -32,8 +44,30 @@ public final class DatabaseHandler {
 
   // TODO: call this method when a user first signs up
   // Method for adding a user to the database
-  public static void addUserToDatabase(User user) {
-    userMap.put(user.getId(), user);    
+  public static void addUserToDatabase(User user, HttpServletRequest request) {
+    userMap.put(user.getId(), user);
+
+    String firstName = request.getParameter("firstName");
+    String lastName = request.getParameter("lastName");
+    int dayBirth = Integer.parseInt(request.getParameter("dayBirth"));
+    int monthBirth = Integer.parseInt(request.getParameter("monthBirth"));
+    int yearBirth = Integer.parseInt(request.getParameter("yearBirth"));
+
+    // TODO: ask Ngan to pass in userService.getCurrentUser()
+    String email = userService.getCurrentUser().getEmail();
+    String id = userService.getCurrentUser().getUserId();
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    Query query = new Query("UserInfo")
+            .setFilter(new Query.FilterPredicate("id", Query.FilterOperator.EQUAL, id));
+    PreparedQuery results = datastore.prepare(query);
+    Entity entity = results.asSingleEntity();
+    entity.setProperty("firstName", firstName);
+    entity.setProperty("lastName", lastName);
+    entity.setProperty("dayBirth", dayBirth);
+    entity.setProperty("monthBirth", monthBirth);
+    entity.setProperty("yearBirth", yearBirth);
+    datastore.put(entity);    
   }
 
   // Method for getting a user object using an id
@@ -42,7 +76,12 @@ public final class DatabaseHandler {
       return userMap.get(id);    
     } else {
       throw new NoSuchElementException();  
-    }    
+    }
+
+    Query query = new Query("UserInfo")
+            .setFilter(new Query.FilterPredicate("id", Query.FilterOperator.EQUAL, id));
+    PreparedQuery results = datastore.prepare(query);        
+
   }
 
   // Method for removing a user from our database
