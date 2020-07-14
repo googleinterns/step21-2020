5375fcac592101d4afa1709011cdba2235c65655
Laila Gamaleldin <laila_gamaleldin@brown.edu>
2020-07-14 16:38:48 +0000

    switching branches

--- step/src/main/java/com/google/sps/servlets/HomepageServlet.java
+++ step/src/main/java/com/google/sps/servlets/HomepageServlet.java
@@ -1,47 +1,47 @@
-// // Copyright 2019 Google LLC
-// //
-// // Licensed under the Apache License, Version 2.0 (the "License");
-// // you may not use this file except in compliance with the License.
-// // You may obtain a copy of the License at
-// //
-// //     https://www.apache.org/licenses/LICENSE-2.0
-// //
-// // Unless required by applicable law or agreed to in writing, software
-// // distributed under the License is distributed on an "AS IS" BASIS,
-// // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// // See the License for the specific language governing permissions and
-// // limitations under the License.
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-// package com.google.sps.servlets;
+package com.google.sps.servlets;
 
-// import java.io.IOException;
-// import java.io.PrintWriter;
-// import com.google.appengine.api.users.UserService;
-// import com.google.appengine.api.users.UserServiceFactory;
-// import javax.servlet.annotation.WebServlet;
-// import javax.servlet.http.HttpServlet;
-// import javax.servlet.http.HttpServletRequest;
-// import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-// @WebServlet("/Homepage")
-// public class HomepageServlet extends HttpServlet {
+@WebServlet("/Homepage")
+public class HomepageServlet extends HttpServlet {
 
-//   @Override
-//   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-//     // Getting the current user's id
-//     String id = userService.getCurrentUser().getUserId();
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    // Getting the current user's id
+    String id = userService.getCurrentUser().getUserId();
 
-//     response.setContentType("text/html");
-//     PrintWriter out = response.getWriter();
+    response.setContentType("text/html");
+    PrintWriter out = response.getWriter();
 
-//     Collection<User> userMatches = DatabaseHandler.getUserMatches();
+    Collection<User> userMatches = DatabaseHandler.getUserMatches();
 
 
-//   }
+  }
 
-//   @Override
-//   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-//   }
+  }
 
-// }
+}
