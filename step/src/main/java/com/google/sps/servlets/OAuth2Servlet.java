c4192a425056f0b9eb52fcd9867b31ddfa5719b8
Adam Samuelson <adamsamuelson@google.com>
2020-07-20 22:11:49 +0000

    Clean up and improve readability in OAuth2Servlet.java

--- step/src/main/java/com/google/sps/servlets/OAuth2Servlet.java
+++ step/src/main/java/com/google/sps/servlets/OAuth2Servlet.java
@@ -12,49 +12,42 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Generic imports
 import java.util.List;
 import java.util.Arrays;
 import java.io.FileReader;
 import java.io.FileNotFoundException;
-
-// Servlet imports
 import java.io.IOException;
+import java.lang.IllegalStateException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-// Authorization imports
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow.Builder;
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeRequestUrl;
-import com.google.api.client.auth.oauth2.AuthorizationCodeTokenRequest;
-import com.google.api.client.auth.oauth2.TokenResponse;
-
-// Users API imports
 import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow.Builder;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeRequestUrl;
+import com.google.api.client.auth.oauth2.AuthorizationCodeTokenRequest;
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.auth.oauth2.TokenResponse;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
 
 @WebServlet("/oauth2")
 public class OAuth2Servlet extends HttpServlet {
 
   private GoogleAuthorizationCodeFlow authFlow;
-  private static String AUTH_REDIRECT_URI = // TODO find a way to get rid of this constant
+  private final static String CLIENT_SECRETS_JSON_PATH = "./WEB-INF/classes/client_secrets.json";
+  private final static String AUTH_REDIRECT_URI = // TODO find a way to get rid of this constant
     "https://8080-7dc48ed0-1a8b-4df6-ba73-e55ccd2fb9ed.us-central1.cloudshell.dev/oauth2";
-    // "http://brenda-ding-pod-step-20.uc.r.appspot.com/oauth2";
     // "https://brenda-ding-pod-step-20.ue.r.appspot.com/oauth2";
-  private static String CLIENT_SECRETS_JSON_PATH = "./WEB-INF/classes/client_secrets.json";
 
   @Override
   public void init() {
-    // https://googleapis.dev/java/google-api-client/latest/com/google/api/client/googleapis/auth/oauth2/GoogleClientSecrets.html
 
+    // Load the client id and secret from client_secrets.json into a GoogleClientSecrets object
     GoogleClientSecrets clientSecret = null;
     try {
       clientSecret =
@@ -65,13 +58,15 @@ public class OAuth2Servlet extends HttpServlet {
     } catch (FileNotFoundException e) { // thrown by FileReader constructor
       printError("Unable to find client_secrets.json at " + CLIENT_SECRETS_JSON_PATH);
     } catch (IOException e) { // thrown by GoogleClientSecrets.load()
-      printError("Unable to create the GoogleClientSecrets");
+      printError("Unable to load the GoogleClientSecrets");
     }
-    // catch (Exception e) {
-      // TODO abort oauth process as authFlow won't be able to be created properly
-      // return; // TODO error handle
-    // }
 
+    if (clientSecret == null) {
+      printError("clientSecret was unable to be initialized. "
+                  + "User won't be able to be authenticated");
+    }
+
+    // Initialize the authorization code flow so we can authenticate the user
     authFlow = new GoogleAuthorizationCodeFlow.Builder(
       new NetHttpTransport(),
       new JacksonFactory(),
@@ -79,80 +74,74 @@ public class OAuth2Servlet extends HttpServlet {
       CalendarManager.getScopes()
     ).build();
 
-    if(authFlow == null) {
-      // TODO error handle
-      printError("authentication flow was unable to be initialized");
+    if (authFlow == null) {
+      printError("Authentication flow was unable to be initialized. "
+                  + "User won't be able to be authenticated");
     }
+
   }
 
   /**
-   * Authorization code flow attempt: https://developers.google.com/api-client-library/java/google-api-java-client/oauth2#authorization_code_flow
+   * Preconditions to this GET request:
+   * - authFlow in init() must have been able to be initialized; AND
+   * - a user must currently be logged in
+   *
+   * GET requests to this servlet are interpreted as requests to authenticate the currently logged
+   * in user and will redirect the browser to our application's OAuth consent screen with Google.
+   * The GoogleAuthroizationCodeFlow used in this method follows the Authorization Code Grant flow
+   * as described here: https://tools.ietf.org/html/rfc6749#section-4.1
+   * This authorization code flow follows the GoogleAuthorizationCodeFlow example found here:
+   * https://developers.google.com/api-client-library/java/google-api-java-client/oauth2#authorization_code_flow
    */
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    // If authCode is not null, the user has been redirected from Google's authorization server.
-    // Else, this is a normal GET request.
+    if (authFlow == null) {
+      throw new IllegalStateException("The user cannot be authenticated without authFlow "
+                                      + "being initialized.");
+    }
+
+    String userId = UserServiceFactory.getUserService().getCurrentUser().getUserId();
+    if (userId == null) {
+      throw new IllegalStateException("Unable to obtain userId while authenticating. "
+                                      + "/oauth2 may only be called while a user is logged in.");
+    }
+
+    // TODO(adamsamuelson): handle when the user rejects oauth request
+    // If authCode is null, the user is beginning the oauth process.
+    // If authCode isn't null, the user has been redirected from Google's authorization server.
     String authCode = request.getParameter("code");
-    if(authCode == null) {
+    if (authCode == null) {
       System.out.println("Beginning authorization code flow...");
 
-      // Authorization code flow:
-      // 1) End user logins into your app. Generate a user ID that is unique for your app
-      UserService userService = UserServiceFactory.getUserService();
-      com.google.appengine.api.users.User userServiceUser = userService.getCurrentUser();
-      String userId = userServiceUser.getUserId();
-      if(userId == null) {
-        // TODO error handle
-        printError("Unable to obtain userId");
-      }
-
-      // 2) Call AuthorizationCodeFlow.loadCredential(String userId) to check if the end-user's
-      //    credentials are already known. If so, we're done.
-      Credential credential = authFlow.loadCredential(userId);
-      if(credential != null) { // If the user's credentials were found,
+      if (authFlow.loadCredential(userId) != null) { // If the user's credentials were found,
         return; // we're done.
       }
 
-      // 3) Call AuthorizationCodeFlow.newAuthoirzationUrl()
-      //    direct the end-user's broweser to an authorization page so they can grant the app
-      //    access to their protected data. Also need to specify the url to redirect back to app
+      // Redirect the user to Google's OAuth consent screen for our application.
       GoogleAuthorizationCodeRequestUrl authCodeRequestUrl = authFlow.newAuthorizationUrl();
       authCodeRequestUrl.setRedirectUri(AUTH_REDIRECT_URI);
-      String authRedirectUrl = authCodeRequestUrl.build();
-      System.out.println("authCodeRequestUrl: " + authCodeRequestUrl.toString());
-      response.sendRedirect(authRedirectUrl);
-      return;
-      // TODO handle when the user rejects oauth request  
-    } else {
-      System.out.println("authCode: " + authCode);
+      response.sendRedirect(authCodeRequestUrl.build());
 
-      // 4) The Google authorization server will redirect the user back to your app, along with a
-      //    code query parameter. Use the code query param to request an access token by calling
-      //    AuthorizationCodeFlow.newTokenRequest(String authcode)
-      // AuthorizationCodeTokenRequest authCodeTokenResponse = authFlow.newTokenRequest(authCode);
+      // Authorization code flow continues in the following else statement.
+      
+    } else {
+      // Upon user's consent, the user redirects to us with a code query parameter that we use
+      // to request an access token from Google. This token will authorize our GCal API requests.
       TokenResponse tokenResponse = authFlow
           .newTokenRequest(authCode)
           .setRedirectUri(AUTH_REDIRECT_URI)
           .execute();
-      System.out.println("after newTokenRequest");
-      System.out.println("tokenResponse: " + tokenResponse);
 
-      // 5) Use AuthorizationCodeFlow.createAndStoreCredential(TokenResponse, String) to store and
-      //    obtain a credential for accessing protected services.
-      //    Use Credential.setAccessToken(String accessToken) ??
-      UserService userService = UserServiceFactory.getUserService();
-      com.google.appengine.api.users.User userServiceUser = userService.getCurrentUser();
-      String userId = userServiceUser.getUserId();
-      // TODO(adamsamuelson): might be a deprecated method, look at setDataStoreFactory() in .Builder
+      // TODO(adamsamuelson): this might be a deprecated method, look at 
+      //    Builder.setDataStoreFactory() or maybe Credential.setAccessToken(String accessToken)
+      // Use the access token to store and obtain a credential to auth our GCal API requests.
       Credential credential = authFlow.createAndStoreCredential(tokenResponse, userId);
-      System.out.println("credential: " + credential);
+
       System.out.println("Authorization code flow complete.");
+      response.sendRedirect("/profile.jsp");
 
       // test the calendar event being created
       CalendarManager.createTestGCalEvent(credential);
-
-      // Done.
-      response.sendRedirect("/profile.jsp");
     }
     
   }
