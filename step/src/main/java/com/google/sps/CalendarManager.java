0fd7a65f755909f24db2bb88d60860c5ae95e538
Adam Samuelson <adamsamuelson@google.com>
2020-07-20 20:23:10 +0000

    Delete unused maven dependencies, move CalendarManager.java
    The only dependency that remains is for the Google Calendar API

--- step/src/main/java/com/google/sps/CalendarManager.java
+++ step/src/main/java/com/google/sps/CalendarManager.java
@@ -27,7 +27,6 @@ import com.google.api.services.calendar.CalendarScopes;
 
 // Authorization imports
 import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.auth.oauth2.Credential;
 
