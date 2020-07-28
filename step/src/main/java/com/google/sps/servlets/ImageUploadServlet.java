94244c63ab7c5b8f998166f7a2f81796f0f9b616
Laila Gamaleldin <laila_gamaleldin@brown.edu>
2020-07-28 17:50:20 +0000

    403 error

--- step/src/main/java/com/google/sps/servlets/ImageUploadServlet.java
+++ step/src/main/java/com/google/sps/servlets/ImageUploadServlet.java
@@ -19,7 +19,8 @@ import java.io.PrintWriter;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;import com.google.appengine.api.blobstore.BlobInfo;
+import javax.servlet.http.HttpServletResponse;
+import com.google.appengine.api.blobstore.BlobInfo;
 import com.google.appengine.api.blobstore.BlobInfoFactory;
 import com.google.appengine.api.blobstore.BlobKey;
 import com.google.appengine.api.blobstore.BlobstoreService;
@@ -35,69 +36,54 @@ import org.json.simple.JSONObject;
 @WebServlet("/blobstore-upload-url")
 public class ImageUploadServlet extends HttpServlet {
 
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+      System.out.println("inside get upload servlet");
+  }
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    System.out.println("do post");
+    System.out.println("inside doPost!!!!!!!!!!!!!!!!!!!!!!!!");
 
     UserService userService = UserServiceFactory.getUserService();
-    System.out.println("1");
     String id = userService.getCurrentUser().getUserId();
-    System.out.println("2");
     String imageBlobKey = filterAndUploadImageBlobstore(request);
-    System.out.println("3");
 
     if (imageBlobKey == null) {
-      System.out.println("is null");
       System.err.println("Error occured while uploading user's image");
       // TODO: Display some message on the screen telling the user to check the file
       //       they uploaded and try again.
     } else {
-      System.out.println("is not null");
       DatabaseHandler.uploadUserImage(id, imageBlobKey);
     }
-    System.out.println("sending redirect");
+    /// line below this is causing an "error 405: HTTP method GET is not supported by this url"
     response.sendRedirect("profile.jsp");
   }
 
   private String filterAndUploadImageBlobstore(HttpServletRequest request){
-    System.out.println("inside filter and upload");
     BlobstoreService myBlobService = BlobstoreServiceFactory.getBlobstoreService();
-    System.out.println("a");
     Map<String, List<BlobKey>>  blobs = myBlobService.getUploads(request);
-    System.out.println("b");
     List<BlobKey> myKeys = blobs.get("image");
-    System.out.println("c");
 
     if (myKeys == null || myKeys.isEmpty()) {
-      System.out.println("returning null 1");
       return null;
     }
 
     BlobKey imageBlobKey = myKeys.get(0);
-    System.out.println("d");
     // Checks if user selected a file to upload
     BlobInfo blobInfo = new BlobInfoFactory().loadBlobInfo(imageBlobKey);
-    System.out.println("e");
     if (blobInfo.getSize() == 0) {
-      System.out.println("f");
       myBlobService.delete(imageBlobKey);
-      System.out.println("g");
       return null;
     }
 
     // Checks if file type is an image
-    System.out.println("h");
     String type = blobInfo.getContentType();
-    System.out.println("i");
     if (!type.startsWith("image")){
-      System.out.println("j");
       myBlobService.delete(imageBlobKey);
-      System.out.println("k");
       return null;
     }
 
-    System.out.println("l");
     return imageBlobKey.getKeyString();    
   }
 
